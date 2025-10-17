c26cadb899d60fd8d604a7ee3475cc11f68a1797
Deniss <cvetkovsdeniss4@gmail.com>
2025-10-17 11:45:19 +0300

    CSS removed from layout to app

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,25 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <style>
-            a{
-                font-style: italic;
-                color: purple;
-            }
-            a:hover{
-                color:darkblue;
-            }
-            p{
-                font-family: Arial;
-                font-size: 14px;
-            }
-            hr{
-                border-style: dashed;
-                border-width: 2px;
-                color: black;
-            }
-            @vite(['resources/css/app.css', 'resources/js/app.js'])
-        </style>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
         <h1>Products</h1>
