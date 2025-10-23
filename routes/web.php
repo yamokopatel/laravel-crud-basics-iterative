b115b14e98752cac508fe01bea50a88e85f678ed
Deniss <cvetkovsdeniss4@gmail.com>
2025-10-23 15:51:58 +0300

    Added redirection to product list from index address

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', function(){
+    return redirect()->route('products.index');
 });
 
 Route::get('/products', [ProductController::class, 'index'])->name('products.index');
