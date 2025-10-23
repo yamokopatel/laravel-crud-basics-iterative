59393b8111638a603c7c263086b6be3ef3d708d0
Deniss <cvetkovsdeniss4@gmail.com>
2025-10-23 15:22:08 +0300

    Modified style using grid, added structure to style

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -2,27 +2,32 @@
     <x-slot:title>
         List
     </x-slot>
-<ul>
-    @foreach ($products as $product)
-        <li>
-            <h2>{{ $product->name }}</h2>
-            <p>{{ $product->description }}</p>
-            <a href="{{ route('products.show', ['product' => $product]) }}">
-                <button>Show</button>
-            </a>
-            <a href="{{ route('products.edit', ['product' => $product]) }}">
-                <button>Edit</button>
-            </a>
-            <form action="{{ route('products.delete', ['product' => $product]) }}" method="post">
-                @csrf
-                @method('DELETE')
 
-                <input type="submit" value="Delete" class="submit">
-            </form>
-        </li>
-    @endforeach
-</ul>
+    <div id="list">
+        <ul>
+            @foreach ($products as $product)
+                <li class="element">
+                    <h2 class="productName">{{ $product->name }}</h2>
+                    <p class="productDesc">{{ $product->description }}</p>
+                    <div class="rANDu">
+                        <a href="{{ route('products.show', ['product' => $product]) }}">
+                            <button>Show</button>
+                        </a>
+                        <a href="{{ route('products.edit', ['product' => $product]) }}">
+                            <button>Edit</button>
+                        </a>
+                    </div>
+                    <form action="{{ route('products.delete', ['product' => $product]) }}" method="post" class="deleteForm">
+                        @csrf
+                        @method('DELETE')
 
-<!-- <a href="/products/create">Create new product</a> -->
+                        <input type="submit" value="Delete" class="deleteSubmit">
+                    </form>
+                </li>
+            @endforeach
+        </ul>
+    </div>
+
+    <!-- <a href="/products/create">Create new product</a> -->
 
 </x-layout>
